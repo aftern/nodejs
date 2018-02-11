057a23d8e0031ac456d022a7c2f8973bf85adaa9
aftern <aftern@nate.com>
2018-02-11 23:25:06 +0900

    nodejs 1st

---
+++ webserver.js
@@ -0,0 +1,15 @@
+const http = require('http'); 
+//http모듈이 필요(require)하다. 리턴값이 있어 상수<const>(http)에 담음
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
