1bbf9979d5b95e50e4672eca30d1a934c1c4a914
aftern <aftern@nate.com>
2018-02-13 07:27:08 +0900

    nodejs 2nd

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+	res.send('Hello home page');
+});
+
+app.get('/dynamic', function(req, res){
+	var lis = '';
+	for(var i=0; i<5; i++){
+		lis = lis + '<li>coding</li>';
+	};
+	var time = Date();
+	var output = `<!doctype html>
+	<html>
+	<head>
+	  <meta charset="utf-8">
+	  <title></title>
+	</head>
+	<body>
+	  Hello, static!
+	  <ul>
+		  ${lis}
+	  </ul>
+	  ${time};
+	  </body>
+	</html>`;
+	res.send(output);
+})
+
+app.get('/chengha', function(req, res){
+	res.send('Hello chengha, <img src="/chengha.jpg">');
+});
+
+app.get('/login', function(req, res){
+	res.send('Login please');
+});
+
+app.listen(3000, function(){
+	console.log('connected 3000 port!');
+});
