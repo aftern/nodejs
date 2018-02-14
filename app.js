84b7a1b99d263db62cd3a6cfb19b195af580c519
aftern <aftern@nate.com>
2018-02-15 06:40:24 +0900

    nodejs 3rd

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 var express = require('express');
 var app = express();
+app.locals.pretty = true;
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/topic', function(req, res){
+	var topics = [
+		'Javascript is...',
+		'Nodejs is...',
+		'Express is...'
+	];
+	var output = `
+		<a href="/topic?id=0">JavaScript</a><br>
+		<a href="/topic?id=1">Nodejs</a><br>
+		<a href="/topic?id=2">Express</a><br><br>
+		${topics[req.query.id]}
+	`
+	res.send(output);
+})
+
+app.get('/template', function(req, res){
+	res.render('temp', {time:Date(), _title:'pug'});
+})
 
 app.use(express.static('public'));
 
