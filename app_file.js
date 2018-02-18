312940107d2015b4e2aea3ae06f3385ff6bafbbd
aftern <aftern@nate.com>
2018-02-19 01:12:29 +0900

    nodejs 5th node-mysql

--- app_file.js
+++ app_file.js
@@ -1,14 +1,33 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var multer = require('multer');
+var _storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+});
+var upload = multer({ storage: _storage});
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.locals.pretty = true;
+app.use('/user', express.static('uploads'));
 app.set('views', './views_file');
 app.set('view engine', 'pug');
 app.listen(3333, function(){
     console.log('conneted, 3333 port!');
 });
+
+app.get('/upload', function(req, res){
+	res.render('upload');
+});
+app.post('/upload', upload.single('userfile'), function(req, res){
+    console.log(req.file);
+    res.send('uploaded : '+ req.file.filename);
+})
 
 app.get('/topic/new', function(req, res){
     fs.readdir('data', function(err, files){
@@ -51,6 +70,6 @@ app.post('/topic', function(req, res){
             console.log(err);
             res.status(500).send('Internal Server Error');
         }
-        res.redirect('topic/'+title);
+       res.redirect('topic/'+title);
     })
 });
---
+++ database_mysql.js
@@ -0,0 +1,55 @@
+var mysql      = require('mysql');
+var conn = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'o2'
+});
+
+conn.connect();
+/* 출력
+var sql = 'SELECT * FROM topic';
+conn.query(sql, function(err, rows, fields){
+    if(err){
+        console.log(err);
+    } else{
+        for(var i=0; i<rows.length; i++){
+            console.log(rows[i].title);
+            console.log(rows[i].description);
+            console.log(rows[i].author);
+        }
+    }
+});
+*/
+/* 삽입
+var sql = 'INSERT INTO topic (title, description, author) VALUES(?, ?, ?)';
+var params = ['Supervisor', 'Watcher', 'Graphittie'];
+conn.query(sql, params, function(err, rows, fields){
+    if(err){
+        console.log(err);
+    } else{
+        console.log(rows.insertId);
+    }
+});
+*/
+/* 수정
+var sql = 'UPDATE topic SET title=?, author=? WHERE id=?';
+var params = ['NPM', 'leezche', '1'];
+conn.query(sql, params, function(err, rows, fields){
+    if(err){
+        console.log(err);
+    } else{
+        console.log(rows);
+    }
+});
+*/
+var sql = 'DELETE FROM topic WHERE id=?';
+var params = ['1'];
+conn.query(sql, params, function(err, rows, fields){
+    if(err){
+        console.log(err);
+    } else{
+        console.log(rows);
+    }
+});
+conn.end();
